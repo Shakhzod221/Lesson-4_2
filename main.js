b5d85300bb57a91cfb87c05ffdb9de87af5abf12
Shakhzod221 <130886474+Shakhzod221@users.noreply.github.com>
2023-08-22 23:35:25 +0500

    Uy ishi 4-2

---
+++ main.js
@@ -0,0 +1,146 @@
+
+
+        // Ternary operator
+        // if else qisqarmasi
+
+        
+    // let age = 15;
+    
+    // let res = age > 18 ? 'can swim' : 'can not swim';
+
+
+    // let res1 = age > 18 ? 'can swim' : age > 16 ? 'can eat smth' : age > 12 ? 'can play ping pong' : 'cant';
+
+    // let res2 = age > 18 ? 'can swim' : `cannot swim at ${age}`;
+
+
+
+        // for
+
+
+        // let students = ['ali', 'vali', 'gani', 'salim', 'shavkat'];
+
+        // for(let i = 0; i < 4; i++){
+        //     console.log(
+        //         students[i]
+        //     );
+        // }
+
+
+
+        // let students = [
+        //     {
+        //         name : 'ali',
+        //         age : 25
+        //     },
+        //     {
+        //         name : 'vali',
+        //         age : 23
+        //     },
+        //     {
+        //         name : 'gani',
+        //         age : 31
+        //     },
+        //     {
+        //         name : 'qani',
+        //         age : 13
+        //     },
+        //     {
+        //         name : 'john',
+        //         age : 43
+        //     },
+        // ];
+
+        // function recommendJob(arr){
+        //     for(let i = 0; i < arr.length; i++){
+        //         if(arr[i].age >= 25){
+        //             console.log(
+        //                 `${students[i].name} you should keep up working`
+        //             );
+        //         }
+        //     }
+        // }
+
+        // recommendJob(students)
+
+
+
+
+        // 1-misol
+
+
+        // let k = 10;
+        // let n = 5;
+
+        // for(let i = 1; i <= n; i++){
+        //     console.log(k);
+        // }
+
+
+        // 2-misol
+
+
+        // let a = 1;
+        // let b = 20;
+
+        // for(let i = a; i <= b; i++){
+        //     console.log(i);
+        // }
+
+
+        // 3-misol
+
+
+        // let a = 1;
+        // let b = 10;
+        // count = 0;
+
+        // for(let i = a; i <= b; i++){
+        //     count += 10000;
+        //     console.log(count);
+        // }
+
+
+        // 4-misol
+
+
+        // let a = 1;
+        // let b = 10;
+        // count = 0;
+
+        // for(let i = a; i <= b; i++){
+        //     count += i;
+        // }
+        // console.log(count);
+
+
+        // 5-misol
+
+
+        // let a = 1;
+        // let b = 20;
+
+        // for(let i = a; i <= b; i++){
+        //     if(i % 2 == 0){
+        //         console.log(i);
+        //     }
+        // }
+
+
+        // 6-misol
+
+
+        // let a = 1;
+        // let b = 20;
+
+        // for(let i = a; i <= b; i++){
+        //     if(i % 2 != 0){
+        //         console.log(i);
+        //     }
+        // }
+        
+
+        // 7-misol
+
+
+        
